c083f234027e43ee8c3c7172d18a18cef4cbcaf8
bogroza <bogroza@EN1411077>
2022-12-18 14:05:00 +0200

    Add project files.

---
+++ Data/Groza_Bogdan_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Groza_Bogdan_Lab2.Models;
+
+namespace Groza_Bogdan_Lab2.Data
+{
+    public class Groza_Bogdan_Lab2Context : DbContext
+    {
+        public Groza_Bogdan_Lab2Context (DbContextOptions<Groza_Bogdan_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Groza_Bogdan_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Groza_Bogdan_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Groza_Bogdan_Lab2.Models.Category> Category { get; set; }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Groza_Bogdan_Lab2.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<BookCategory>? BookCategories { get; set; }
+    }
+}
